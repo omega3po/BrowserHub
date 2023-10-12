84e9ebdea38a090f6e83f39c6d4ae99561bd5b81
Sunwoo <sunnyhwang0621@gmail.com>
2023-10-11 21:10:17 -0500

    Initial Commit

---
+++ BrowserHub/BrowserHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserHubApp.swift
+//  BrowserHub
+//
+//  Created by Sunny Hwang on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BrowserHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
